b5c27b87df6140f4af62394f4a734454c6665763
amit <amit28vij@gmail.com>
2020-08-14 16:10:12 +0530

    Added Members Context Provider
    Signed-off-by: amit <amit28vij@gmail.com>

---
+++ src/Context/MembersProvider.tsx
@@ -0,0 +1,60 @@
+import React, { createContext, useReducer, FC } from "react";
+
+interface IMember {
+    name: string;
+    id: number;
+}
+
+interface IMemberState {
+    members: IMember[];
+    idCount: number;
+}
+
+const memberState = createContext<{
+    state: IMemberState;
+    dispatch: React.Dispatch<any>;
+}>({ state: { members: [], idCount: 0 }, dispatch: () => null });
+
+interface IMemberAction {
+    type: string;
+    payload: IMember;
+}
+
+export const MemberActions = {
+    ADD_MEMBER: "ADD_MEMBER",
+    DEL_MEMBER: "DEL_MEMBER",
+};
+
+const memberReducer = (state: IMemberState, action: IMemberAction) => {
+    switch (action.type) {
+        case MemberActions.ADD_MEMBER:
+            return {
+                members: [action.payload, ...state.members],
+                idCount: state.idCount++,
+            };
+        case MemberActions.DEL_MEMBER:
+            return {
+                members: state.members.filter(
+                    (m) => m.id !== action.payload.id
+                ),
+                idCount: state.idCount,
+            };
+        default:
+            return state;
+    }
+};
+
+const MembersProvider: FC<{ children: React.ReactNode }> = (props) => {
+    const [state, dispatch] = useReducer(memberReducer, {
+        members: [],
+        idCount: 0,
+    });
+
+    return (
+        <memberState.Provider value={{ state, dispatch }}>
+            {props.children}
+        </memberState.Provider>
+    );
+};
+
+export default MembersProvider;
