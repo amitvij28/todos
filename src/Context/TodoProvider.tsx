23efc8c9c0ba1c1f0d7d88ce7ff41abefc332c73
amit <amit28vij@gmail.com>
2020-08-14 16:09:24 +0530

    Edited TodoProvider     - Changed enums to objects     - Added actions
    Signed-off-by: amit <amit28vij@gmail.com>

--- src/Context/TodoProvider.tsx
+++ src/Context/TodoProvider.tsx
@@ -1,46 +1,73 @@
 import React, { createContext, FC, useReducer } from "react";
 
-export enum TodoStatus {
-    done = "Done",
-    doing = "Doing",
-    tbd = "To Be Done",
-}
+export const TodoStatus = {
+    done: "Done",
+    doing: "Doing",
+    tbd: "To Be Done",
+};
 
-export enum TodoActions {
-    ADD_TODO = "ADD_TODO",
-    DEL_TODO = "DEL_TODO",
-    EDIT_TODO = "EDIT_TODO",
-}
+export const TodoActions = {
+    ADD_TODO: "ADD_TODO",
+    DEL_TODO: "DEL_TODO",
+    EDIT_TODO: "EDIT_TODO",
+};
 
 export interface ITodo {
     title: string;
     description: string;
     schedule: Date;
     tags: string[];
-    status: TodoStatus;
+    status: string;
     id: number;
     members: string[];
 }
 
+interface ITodoState {
+    todos: ITodo[];
+    idCount: number;
+}
+
 export const todos = createContext<{
-    state: ITodo[];
+    state: ITodoState;
     dispatch: React.Dispatch<any>;
-}>({ state: [], dispatch: () => null });
+}>({ state: { todos: [], idCount: 0 }, dispatch: () => null });
 
 interface ITodoAction {
     type: string;
-    payload: {};
+    payload: ITodo;
 }
 
-const todoReducer = (state: ITodo[], action: ITodoAction) => {
+const todoReducer = (state: ITodoState, action: ITodoAction) => {
     switch (action.type) {
+        case TodoActions.ADD_TODO:
+            return {
+                todos: [...state.todos, action.payload],
+                idCount: state.idCount++,
+            };
+        case TodoActions.DEL_TODO:
+            return {
+                todos: state.todos.filter((t) => t.id === action.payload.id),
+                idCount: state.idCount,
+            };
+        case TodoActions.EDIT_TODO:
+            return {
+                todos: state.todos.map((m) => {
+                    if (m.id === action.payload.id) return action.payload;
+                    return m;
+                }),
+                idCount: state.idCount,
+            };
+
         default:
             return state;
     }
 };
 
 const TodoProvider: FC<{ children: React.ReactNode }> = (props) => {
-    const [state, dispatch] = useReducer(todoReducer, []);
+    const [state, dispatch] = useReducer(todoReducer, {
+        todos: [],
+        idCount: 0,
+    });
 
     return (
         <todos.Provider value={{ state, dispatch }}>
